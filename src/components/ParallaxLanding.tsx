ba9906563ca7b47f3e70831ccd43a51f443999c8
Sam G <34076743+SamGuzel@users.noreply.github.com>
2025-02-12 23:43:45 +0000

    desktop view fix

--- src/components/ParallaxLanding.tsx
+++ src/components/ParallaxLanding.tsx
@@ -383,7 +383,7 @@ export const ParallaxLanding = () => {
               <h3 className="text-2xl font-primary uppercase">Arma Reforger</h3>
 
               {/* Drill Instructors */}
-              <div className="flex flex-col gap-4">
+              <div className="flex gap-4 overflow-x-auto pb-2">
                 <DrillInstructorCard name="Drill Instructor 1" />
                 <DrillInstructorCard name="Drill Instructor 2" />
                 <DrillInstructorCard name="Drill Instructor 3" />
@@ -431,7 +431,7 @@ export const ParallaxLanding = () => {
               <h3 className="text-2xl font-primary uppercase">Arma III</h3>
 
               {/* Drill Instructors */}
-              <div className="flex flex-col gap-4">
+              <div className="flex gap-4 overflow-x-auto pb-2">
                 <DrillInstructorCard name="Drill Instructor 1" />
                 <DrillInstructorCard name="Drill Instructor 2" />
                 <DrillInstructorCard name="Drill Instructor 3" />
